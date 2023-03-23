f49314e704563a3fa783744ddf13e970210bf0a9
lucasbclls <luccasbclls2000@gmail.com>
2023-03-23 17:25:07 -0300

    update project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,50 +1,78 @@
 <template>
-  <div>
-    <v-data-table :headers="headers" :items="desserts" :items-per-page="5" class="elevation-1"></v-data-table>
+  <v-simple-table>
+    <template v-slot:default>
+      <thead>
+        <input
+          class="placeholder:italic placeholder:text-slate-400 block bg-white w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+          placeholder="Search for crypto Select..." type="text" name="search" @keyup="searchCoin()"
+          v-model="textSearch" />
 
-  </div>
+        <tr>
+          <th> Position</th>
+          <th>Coin</th>
+          <th>
+            Name
+          </th>
+          <th>
+            Symbol
+          </th>
+          <th>
+            Current Price
+          </th>
+          <th>
+            Price Change (24h)
+          </th>
+          <th>
+            Market Cap
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(coins, index) in filteredCoins" :key="coins.id">
+          <td>{{ index + 1 }} </td>
+          <td> <img :src="coins.image" style="width: 2rem"> </td>
+          <td>{{ coins.id.toUpperCase() }}</td>
+          <td>{{ coins.symbol }}</td>
+          <td>{{ coins.current_price }}</td>
+          <td :class="[coins.market_cap_change_24h > 0 ? 'text-green-500' : 'text-red-600',]">
+            {{ coins.market_cap_change_24h }} %
+          </td>
+          <td>$ {{ coins.market_cap.toLocaleString() }}</td>
+        </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
 </template>
 
 <script>
 
+import api from '@/services/api.js'
+
 export default {
   name: 'Home',
-
   data() {
     return {
-      headers: [
-        {
-          text: 'Coin',
-          align: 'start',
-          sortable: false,
-          value: 'name',
-        },
-        { text: 'Position', value: 'protein' },
-        { text: 'Price', value: 'calories' },
-        { text: 'Price Change', value: 'fat' },
-        { text: '24h Value', value: 'carbs' },
-      ],
-      desserts: [{
-        name: 'Frozen Yogurt',
-        calories: 159,
-        fat: 6.0,
-        carbs: 24,
-        protein: 20,
-        iron: 1,
-      },
-
-      ]
-     
+      coin: [],
+      filteredCoins: [],
+      textSearch: "",
     }
-  }
+  },
+  mounted() {
+    api.get('/coins').then(response => {
+      this.coin = response.data
+      this.filteredCoins = response.data
+    })
+  },
+  methods: {
+    searchCoin() {
+      this.filteredCoins = this.coin.filter((coin) => 
+      coin.name.toLowerCase().includes(this.textSearch.toLowerCase()) ||
+      coin.symbol.toLowerCase().includes(this.textSearch.toLowerCase()) );
+    }
+  },
+
+
 }
-  
-  
-
-  
-
-
- 
 
 </script>
 
--- src/views/consultChain.vue
+++ src/views/consultChain.vue
@@ -70,5 +70,118 @@
         margin:0;
         padding: 0;
       }
-      </style> -->
+      </style> 
+      <template>
+        
+      </template>
+      
+      <script>
+      export default {
+        methods:{
+        listCoins(){
+          this.loading = true;
+          this.$http.get(
+            "coins/markets?vs_currency=brl&ids=bitcoin%2C%20dacxi%2C%20ethereum%2C%20cosmos"
+          )
+          .then(resp => {
+            this.loading = true;
+            if(resp.status == 200){
+              this.coins = resp.data.map(coin => {
+                return {
+                  ...coin, 
+                  current_price: `R$ ${coin.current_price.toLocaleString()}`,
+                  price_change_24h: parseFloat(`${coin.price_change_24h.toFixed(2)}`),
+                  // price_change_24h: 2,
+                  market_cap: `R$ ${coin.market_cap.toLocaleString()}`,
+                };
+              })
+              this.coinsName = resp.data
+            } else{
+              console.log(resp.message)
+            }
+          }).finally(()=> this.loading = false)
+        },
+        search(){
+          this.loading = true;
+          this.$http.get(
+            `coins/${this.selectedCoin}/history?date=${this.date}&localization=brl`
+          ).then(resp => {
+            const coin = resp.data;
+            if(!coin.market_data){
+              this.snackbar(
+                "Data not found", 
+                "error"
+              )
+            }
+            this.coins = [{
+              name: coin.name,
+              symbol: coin.symbol,
+              current_price: `R$ ${coin.market_data.current_price.brl.toFixed(2).toLocaleString()}`,
+              price_change_24h: "- ",
+              market_cap: `R$ ${coin.market_data.market_cap.brl.toFixed(2).toLocaleString()}`,
+              image: coin.image.small
+            }]
+          }).catch((resp) => {
+              this.snackbar(
+                resp.response.data.error, 
+                "error"
+              )
+            }
+          ).finally(() => this.loading = false)
+        },
+        snackbar(message, color){
+          this.snackbarOptions = {
+            active: true,
+            message,
+            color 
+          }
+        }
+      },
 
+      }
+      </script>
+      
+      <style>
+      
+      </style>
+
+    -->
+    <template>
+      <v-simple-table>
+        <template v-slot:default>
+          <thead>
+            <tr>
+              <th>Coin</th>
+              <th>
+                Name
+              </th>
+              <th >
+                Symbol
+              </th>
+              <th >
+               Current Price
+              </th>
+              <th>
+                Price Change (24h)
+              </th>
+              <th>
+                Market Cap
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="coins in coin"
+              :key="coins.name"
+            > <td> <img :src="coins.image" style="width: 2rem"> </td>
+              <td>{{ coins.id.toUpperCase() }}</td>
+              <td>{{ coins.symbol }}</td>
+              <td>{{ coins.current_price }}</td>
+              <td>{{ coins.market_cap_change_24h}}</td>
+              <td>{{ coins.market_cap }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
+    </template>
+    
